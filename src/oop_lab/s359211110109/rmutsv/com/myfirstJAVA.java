bfb2919200d7b2359d3480bee3ec9c367e02c53a
sittidamrong <nakarin2539f@gmail.com>
2017-11-17 11:14:14 +0700

    successfuly information data

---
+++ src/oop_lab/s359211110109/rmutsv/com/myfirstJAVA.java
@@ -0,0 +1,13 @@
+package oop_lab.s359211110109.rmutsv.com;
+
+public class myfirstJAVA {
+    public static void main(String[] args) {
+        System.out.println("Hello Wordl, I'm FEEM");
+        System.out.println("Information System");
+        System.out.println("Faculty of managemant Technololgy");
+        System.out.println("RMUTSV");
+    }//main
+
+
+
+}
