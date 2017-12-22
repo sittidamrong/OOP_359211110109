304112b6f16889ef02a064166781653f2741d1c9
LAB_COM-MT_1 <nakarin2539f@gmail.com>
2017-12-22 10:34:21 +0700

    myArray

---
+++ src/ooplab5/InputDatatoArray.java
@@ -0,0 +1,41 @@
+package ooplab5;
+
+import java.security.PrivateKey;
+import java.util.Scanner;
+
+public class InputDatatoArray {
+    private static int MAX = 5;
+    public static void main(String[] args) {
+        int number[]= new int[MAX];
+        //System.out.println(number.length);
+        //for
+        number = inputData(number);
+       showarray(number);
+        showDataEnchance(number);
+    }//main
+
+    private static void showDataEnchance(int[] number) {
+        System.out.println("Data in array: ");
+        for (int val:number)
+        System.out.print(val+" ");
+    }
+
+
+    private static void showarray(int[] number) {
+        System.out.println("Data in Array: ");
+        for (int i=0;i<number.length;i++)
+            System.out.print("number[" + i + "]:");
+    }
+
+
+
+            private static int[] inputData(int[]number){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an integer:");
+        for (int i=0;i<number.length;i++){
+         number[1]= scanner.nextInt();
+
+        }
+        return number;
+    }
+}//class
